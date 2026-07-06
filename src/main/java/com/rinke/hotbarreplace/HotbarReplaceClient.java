c1ec238a4516701129b83a56e9adc8c47adaa83b
rinke <68131839+rinkeChillow@users.noreply.github.com>
2026-07-06 14:59:06 +0100

    Initial commit

---
+++ src/main/java/com/rinke/hotbarreplace/HotbarReplaceClient.java
@@ -0,0 +1,10 @@
+package com.rinke.hotbarreplace;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HotbarReplaceClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
